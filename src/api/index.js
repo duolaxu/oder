74be5ffdf9996635292a603d2bc86870149d0850
duolaxu <2935621631@qq.com>
2022-11-03 12:33:32 +0800

    feat: 删除不相关文件

--- src/api/index.js
+++ src/api/index.js
@@ -6,66 +6,6 @@ export const login = async (path, data) => {
     return res;
 }
 
-// 获取商户列表
-export const getMerchantList = async path => {
-    const res = await axios.get(`${baseUrl}${path}`);
-    return res;
-}
-
-// 获取顾客列表
-export const getCustomerList = async (path, data) => {
-    const res = axios.post(`${baseUrl}${path}`, data);
-    return res;
-}
-
-// 获取菜品列表
-export const getDishList = async (path, data) => {
-    const res = axios.post(`${baseUrl}${path}`, data);
-    return res;
-}
-
-// 更新菜品信息
-export const updateDish = async (path, data) => {
-    const res = axios.post(`${baseUrl}${path}`, data);
-    return res;
-}
-
-// 获取商铺列表
-export const getStoreList = async path => {
-    const res = axios.get(`${baseUrl}${path}`);
-    return res;
-}
-
-// 新增商铺
-export const addNewStore = async (path, data) => {
-    const res = axios.post(`${baseUrl}${path}`, data);
-    return res;
-}
-
-// 新增食品
-export const addNewDish = async (path, data) => {
-    const res = axios.post(`${baseUrl}${path}`, data);
-    return res;
-}
-
-// 菜品状态
-export const changeDishStatus = async (path, data) => {
-    const res = axios.post(`${baseUrl}${path}`, data);
-    return res;
-}
-
-// 获取食品分类列表
-export const getDishTypeList = async (path, data) => {
-    const res = axios.post(`${baseUrl}${path}`, data);
-    return res;
-}
-
-// 更新菜品序号
-export const updateDishSerial = async (path, data) => {
-    const res = axios.post(`${baseUrl}${path}`, data);
-    return res;
-}
-
 // 获取租房信息表
 export const getHouse = async (path, data) => {
     const res = axios.post(`${baseUrl}${path}`, data);
--- src/page/manage/index.vue
+++ src/page/manage/index.vue
@@ -11,14 +11,9 @@
           active-text-color="#3190e8"
         >
           <el-menu-item index="home"> 首页</el-menu-item>
-          <!-- <el-menu-item index="merchantList">商户列表</el-menu-item>
-          <el-menu-item index="customerList">顾客列表</el-menu-item>
-          <el-menu-item index="foodList">食品列表</el-menu-item>
-          <el-menu-item index="shopList">商铺列表</el-menu-item> -->
           <el-menu-item index="recentHouse">房屋信息</el-menu-item>
           <el-menu-item index="secondHand">二手物品</el-menu-item>
           <el-menu-item index="recruitment">职位信息</el-menu-item>
-          <!-- <el-menu-item index="incomeChart">收入流水</el-menu-item> -->
         </el-menu>
       </el-col>
       <el-col :span="20" style="height: 100%; overflow: auto">
@@ -53,14 +48,8 @@ export default {
 
 
 <style scoped>
-/* #elCol{
-    background-color: #324057 !important;
-  } */
 .elMenu {
   background-color: #324057 !important;
   color: white !important;
 }
-/* el-menu-item{
-  color:white;
-} */
 </style>
